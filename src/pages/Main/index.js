e70b684f4cc83a94aa98655dfd068405bbcade81
alexavier20 <alex_xavier@outlook.com>
2020-02-07 16:54:45 -0300

    first commit

---
+++ src/pages/Main/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Container } from './styles';
+import api from '../../services/api';
+
+export default function Main() {
+    return (
+        <Container>
+            <h1>Main</h1>
+        </Container>
+    );
+}
